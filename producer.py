6411292eef1e8aee88cc30725f52bc41fdf5180a
HadiAbu <hadi.abuhamed@gmail.com>
2026-01-26 12:16:39 +0200

    everything setup with producer ready

---
+++ producer.py
@@ -0,0 +1,31 @@
+import json
+from confluent_kafka import Producer
+from uuid import uuid4
+
+producerConfig = {"bootstrap.servers": "localhost:9092"}
+
+producer = Producer(producerConfig)
+
+
+# Callback function to handle delivery reports
+def delivery_report(err, msg):
+    """Called once for each message produced to indicate delivery result.
+    Triggered by poll() or flush()."""
+    if err is not None:
+        print(f"❌ Message delivery failed: {err}")
+    else:
+        print(
+            f"✅ Message delivered to: {msg.topic()}, partition: [{msg.partition()}], offset: {msg.offset()}"
+        )
+
+
+order = {"order_id": str(uuid4()), "user": "Bob", "item": "laptop", "quantity": 1}
+
+# turn the order dictionary into a JSON string and then encode it to bytes
+orderStr = json.dumps(order).encode("utf-8")
+
+# SEND TO: Kafka "orders" topic
+producer.produce(topic="orders", value=orderStr, callback=delivery_report)
+
+# Ensure all messages are sent before exiting ensuring durability
+producer.flush()
